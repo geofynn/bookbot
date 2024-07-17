256ebb8f8b60aa974e6022002ba1d32749b6e5aa
geofynn <Fynn.Barmwater1@gmail.com>
2024-07-18 01:45:56 +0200

    Added find word count and chracter count

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-with open("/books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+def get_file(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(file):
+    file_as_string = file.split()
+    counter = 0
+    for word in file_as_string:
+        counter += 1
+    return counter
+
+def character_count(file):
+    character_dict = {}
+    file = file.lower()
+    file_as_array = file.split()
+    #print(file_as_array)
+    for word in file_as_array:
+        for character in word:
+            if character in character_dict:
+                character_dict[character] += 1
+            else:
+                character_dict[character] = 1
+    return character_dict
+    
+
+def main():
+    path = "books/frankenstein.txt"
+    read_file = get_file(path)
+    word_count = count_words(read_file)
+    print(word_count)
+    character_dict = character_count(read_file)
+    print(character_dict)
+        
+
+main()
