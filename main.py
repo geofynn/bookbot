32f7c7047ba86d37dd7b574c774983daa4bee73b
geofynn <Fynn.Barmwater1@gmail.com>
2024-07-18 12:04:55 +0200

    finalish version

--- main.py
+++ main.py
@@ -22,15 +22,31 @@ def character_count(file):
             else:
                 character_dict[character] = 1
     return character_dict
-    
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_Report(word_count, character_dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    character_list = character_dict.items()
+    dict_array = []
+    for item in character_list:
+        if item[0].isalpha() == True:
+            dict_array.append({"character": f"{item[0]}", "num": item[1]})
+    dict_array.sort(reverse=True, key=sort_on)
+    for item in dict_array:
+        print("The ", item["character"], " character was found ", item["num"],  " times")
+    print("--- End report ---")
+
+
 
 def main():
     path = "books/frankenstein.txt"
     read_file = get_file(path)
     word_count = count_words(read_file)
-    print(word_count)
     character_dict = character_count(read_file)
-    print(character_dict)
+    print_Report(word_count, character_dict)
         
 
 main()
